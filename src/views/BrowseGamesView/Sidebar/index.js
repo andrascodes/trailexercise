e2dc632a4afbf5c75e91e69a423926e60a46fc51
Andrew Szucs <andrew0szucs@gmail.com>
2018-03-18 17:00:52 +0100

    feat(browse): Add animation to sidebar when it appears

--- src/views/BrowseGamesView/Sidebar/index.js
+++ src/views/BrowseGamesView/Sidebar/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import './Sidebar.css'
+import './SidebarNew.css'
 
 import PlayedByFriendsThumbnail from './PlayedByFriendsThumbnail'
 
@@ -8,18 +8,20 @@ const Sidebar = props => {
   return (
     <div className="Sidebar">
 
-      <h3>What you're friends are playing</h3>
+      <div className="SidebarContent">
+        <h3>What you're friends are playing</h3>
 
-      <div className="PlayedByFriendsList">
-        {props.games.map(({ id, attributes }) => (
-          <PlayedByFriendsThumbnail 
-            key={`playedby-${id}`}
-            name={attributes.name}
-            image={attributes.img_card_bg}
-            backgroundColor={attributes.img_card_avg_color}
-            friends={attributes.online_friends}
-          />
-        ))}
+        <div className="PlayedByFriendsList">
+          {props.games.map(({ id, attributes }) => (
+            <PlayedByFriendsThumbnail 
+              key={`playedby-${id}`}
+              name={attributes.name}
+              image={attributes.img_card_bg}
+              backgroundColor={attributes.img_card_avg_color}
+              friends={attributes.online_friends}
+            />
+          ))}
+        </div>
       </div>
 
     </div>
